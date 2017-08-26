edbe4bb2abffb23a1cdf1a6823804fb2d08b91b8
Justin Ley <inkysigma@outlook.com>
2017-08-26 00:47:35 -0700

    Prototyped model.

--- recommender/learner/learner.py
+++ recommender/learner/learner.py
@@ -8,7 +8,8 @@ class Learner:
 
     def __init__(self,
                  activation_function: Callable[tf.Tensor, tf.Tensor]=tf.nn.relu,
-                 name: str="track-learner"):
+                 name: str="track-learner",
+                 categories: int= 10):
         """
         Creates a learner with the given activation function and name.
         Args:
@@ -29,44 +30,94 @@ class Learner:
 
         # Create a counter for global_steps
         self.global_step = tf.Variable(
-            0, dtype=int32, trainable=False, name="global_step")
+            0, dtype=tf.int32, trainable=False, name="global_step")
 
         # Specify that the graph is unbuilt
         self.__graph_built__ = False
 
+        # Create a system of weights and biases
+        self.__initialize_variables__()
+
+        self.__categories__ = categories
+
+    def __initialize_variables__(self):
+        """Initialize the variables needed to multiply matricies and add biases"""
+        self.__weights__ = [
+            self.__create_weight__([1024, 512]),
+            self.__create_weight__([512, 256]),
+            self.__create_weight__([256, 128]),
+            self.__create_weight__([128, 64]),
+            self.__create_weight__([64, 64])
+        ]
+        self.__biases__ = [
+            self.__create_bias__([512]),
+            self.__create_bias__([256]),
+            self.__create_bias__([128]),
+            self.__create_bias__([64]),
+            self.__create_bias__([64])
+        ]
+
+    def __create_weight__(self, shape, name: str=None):
+        return tf.Variable(tf.truncated_normal(shape, stddev=0.1), name=name)
+
+    def __create_bias__(self, shape, name:str=None):
+        return tf.Variable(tf.constant(0.2, shape), name=name)
+
     def train(self, batch, y):
         if not self.__graph_built__:
-            build_graph()
-        pass
+            self.build_graph()
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=self.__final_layer__))
+        train_step = tf.train.GradientDescentOptimizer(
+            0.5).minimize(cross_entropy)
+        train_step.run(feed_dict={self.X: batch, self.y: y})
 
     def test(self, batch, y):
         pass
 
     def load(self, file: str):
         """
-        Loads all the variables from the 
+        Loads all the variables from a file
         """
+        saver = tf.train.Saver()
+        saver.restore(self.sess, file)
 
-    def load_saved(self, name: str) -> None:
-        if self.__graph_built__:
-            return
-
-        self.__graph_built__ = True
-
-    def build_graph(self):
+    def build_graph(self) -> None:
         """
-        Builds the variables in the graph. Should be called before running
-        Args:
-            batch_size (int): the number of batches passed in per train or test
+        Builds the variables in the graph. Should be called before running.
         """
 
         # Inputs to our models
-        X_ = tf.placeholder(dtype=tf.float32, shape=(None, 1024), name="X_")
+        self.X = tf.placeholder(dtype=tf.float32, shape=(None, 1024), name="X")
+
+        # Batch size
+        batch_size = tf.shape(X)[0]
 
         # Target outputs of our model
-        y_ = tf.placeholder(dtype=tf.float32, shape=(None, 1), name="y_")
+        self.y = tf.placeholder(dtype=tf.float32, shape=(None, 1), name="y")
 
-        conv1 = tf.nn.conv1d(placeholders, filters=32, stride=10, padding="SAME", name="conv1")
+        conv1 = tf.nn.conv1d(X, filters=32, stride=10,
+                             padding="SAME", name="conv1")
+
+        maxpool1 = tf.nn.max_pool(conv1, ksize=[batch_size, 1, 1024, 1], strides=[
+                                  1, 1, 5, 1], padding="SAME", name="maxpool1")
+
+        dropout1 = tf.nn.dropout(maxpool1, keep_prob=0.4)
+
+        result1 = self.acf(dropout1)
+
+        assert len(self.__weights__) == len(self.__biases__)
+        prev_layer = result1
+        for i in len(self.__weights__):
+            prev_layer = self.acf(
+                tf.matmul(prev_layer, self.__weights__[i]) + self.__biases__[i])
+
+        final_weight = self.__create_weight__([64, self.__categories__], name="final_weight")
+        final_biases = self.__create_bias__([64], name="final_bias")
+
+        self.__final_layer__ = tf.matmul(
+            prev_layer, final_weight) + final_biases
+        self.__graph_built__ = True
 
     def initialize(self):
         tf.global_variables_initializer()
@@ -82,7 +133,7 @@ class Learner:
 
     def get_global_steps(self) -> int:
         with tf.Session() as temp_sess:
-            return temp_session.run(self.global_step)
+            return temp_sess.run(self.global_step)
 
     def predict(self, batch):
         pass
