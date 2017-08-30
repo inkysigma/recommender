c5b9b10442f2d17cfaff1cbe11df05f3807c9a77
Justin Ley <inkysigma@outlook.com>
2017-08-30 10:52:20 -0700

    Added stub for management

--- recommender/__main__.py
+++ recommender/__main__.py
@@ -2,7 +2,12 @@
 import argparse
 
 
-def configure(*files: str) -> (argparse.Namespace, dict):
+def configure(*files: str) -> argparse.Namespace:
+    """
+    Configure the application for usage.
+    Returns:
+        argparse.Namspace: the namespace containing the arguments parsed from the command line
+    namespace"""
     parser = argparse.ArgumentParser()
     learner_parser = parser.add_subparsers(
         help="Select an option to either train/test/predict.")
@@ -10,11 +15,18 @@ def configure(*files: str) -> (argparse.Namespace, dict):
     training_parser = learner_parser.add_parser("train")
     training_parser.set_defaults(method="train")
 
+    # TODO: Add methods to build the model like turning on GPU support.
+    # Creating GPU groups and using them
+    # Selecting files to load and save to
+
     test_parser = learner_parser.add_parser("test")
     test_parser.set_defaults(method="test")
 
     predict_parser = learner_parser.add_parser("predict")
     predict_parser.set_defaults(method="predict")
+
+    manage_parser = learner_parser.add_parser("manage")
+    manage_parser.set_defaults(method="manage")
 
     flags = parser.parse_args()
 
